7d542470fc1a89d9ae4766f926d5921b7388c8ad
Cristian Buitrago <cristian@Cristians-MacBook-Pro.local>
2025-07-07 16:23:10 -0500

    feat: css styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -41,12 +41,12 @@ export default function Home() {
           [`Question ${index + 1}`]:
             item.text || item.choice?.label || '[ERROR]',
         }),
-        { 0: title }
+        { ['Question 0']: title }
       )
     );
 
     const csv = unparse(processData, {
-      delimiter: ';'
+      delimiter: ';',
     });
     const BOM = '\uFEFF';
     const blob = new Blob([BOM + csv], { type: 'text/csv;charset=utf-8;' });
@@ -61,89 +61,63 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/Talent-Logo.png"
-          alt="Talent logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1>Talent Group TypeForm Information Process</h1>
-        <input
-          type="text"
-          name="workspaceID"
-          id="workspaceID"
-          placeholder="workspace ID"
-          value={inputValue}
-          onChange={handleInputChange}
-          className="max-w-[250px] px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
-        />
-        <button
-          onClick={handleSubmit}
-          className="px-6 py-2 bg-gradient-to-r from-blue-600 to-indigo-600 text-white rounded-xl shadow-md hover:from-blue-700 hover:to-indigo-700 transition-all duration-300 ease-in-out transform hover:scale-105"
-        >
-          Submit
-        </button>
-        {!!data?.length && (
+    <div className="flex flex-col p-10 h-screen w-screen">
+      <main className="flex flex-col grow gap-[14px] md:gap-[32px] row-start-2 items-center sm:items-start h-full w-full">
+        <div className="flex justify-center items-center md:items-baseline gap-[30px]">
+          <Image
+            className="dark:invert"
+            src="/Talent-Logo.png"
+            alt="Talent logo"
+            width={180}
+            height={38}
+            priority
+          />
+          <h1 className="sm:text-1xl md:text-2xl font-semibold text-gray-800 mb-4 border-b border-gray-300 pb-2">
+            Talent Group TypeForm Information Process
+          </h1>
+        </div>
+        <div className="flex flex-wrap justify-center items-baseline gap-[10px]">
+          <input
+            type="text"
+            name="workspaceID"
+            id="workspaceID"
+            placeholder="workspace ID"
+            value={inputValue}
+            onChange={handleInputChange}
+            className="max-w-[250px] px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all duration-200"
+          />
           <button
+            onClick={handleSubmit}
             className="px-6 py-2 bg-gradient-to-r from-blue-600 to-indigo-600 text-white rounded-xl shadow-md hover:from-blue-700 hover:to-indigo-700 transition-all duration-300 ease-in-out transform hover:scale-105"
-            onClick={handleDownload}
           >
-            Exportar CSV
+            Submit
           </button>
+          {!!data?.length && (
+            <button
+              className="px-6 py-2 bg-gradient-to-r from-blue-600 to-indigo-600 text-white rounded-xl shadow-md hover:from-blue-700 hover:to-indigo-700 transition-all duration-300 ease-in-out transform hover:scale-105"
+              onClick={handleDownload}
+            >
+              Exportar CSV
+            </button>
+          )}
+        </div>
+        <p className="text-xs text-gray-500">
+          Para agregar varios workspaces colocar su ID separado por una coma,
+          ej: NhY5t2, vnvDcM. Asegurarse que todos los cuestionarios de cada
+          worspace tengan la misma cantidad de preguntas.
+        </p>
+
+        {!!data?.length && (
+          <div className="w-full overflow-scroll border border-black rounded-lg shadow-sm">
+            <Table data={data} />
+          </div>
         )}
-        {!!data?.length && <Table data={data} />}
         {!!data?.error && <h1>{data.error}</h1>}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="py-4 row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <p className="flex items-center gap-2 hover:underline hover:underline-offset-4 text-weight-700 text-xs text-gray-300">
+          CB-2025 ©
+        </p>
       </footer>
     </div>
   );
--- src/app/table.jsx
+++ src/app/table.jsx
@@ -14,13 +14,13 @@ const ResponsesTable = ({ data }) => {
           titulos llamas a https://api.typeform.com/forms/VS1ER6yy en el campo
           fields se obtienen los titulos que suelen ser muy largos por eso no se
           usan */}
-          <th className="border border-gray-300 min-w-[200px] h-[40px] max-h-[40px] overflow-y-auto overflow-x-hidden whitespace-normal px-3 py-2 align-top">
-            PERSON
+          <th className="border border-gray-300 min-w-[200px] h-[40px] px-3 py-2 align-top sticky top-0 bg-gray-100">
+            Person
           </th>
           {data[0]?.answers.map(({ field: { id } }, index) => (
             <th
               key={id}
-              className="border border-gray-300 min-w-[200px] h-[40px] max-h-[40px] overflow-y-auto overflow-x-hidden whitespace-normal px-3 py-2 align-top"
+              className="border border-gray-300 min-w-[200px] h-[40px]  px-3 py-2 align-top sticky top-0 bg-gray-100"
             >
               {`Question ${index + 1}`}
             </th>
@@ -33,14 +33,14 @@ const ResponsesTable = ({ data }) => {
             <tr key={index}>
               <td
                 key={index}
-                className="border border-gray-300 min-w-[200px] h-[80px] max-h-[80px] overflow-y-auto overflow-x-hidden whitespace-normal px-3 py-2 align-top"
+                className="border border-gray-300 min-w-[200px] h-[80px] max-h-[80px] px-3 py-2 align-top"
               >
                 {item.title || 'ERROR'}
               </td>
               {item?.answers.map((item, index) => (
                 <td
                   key={index}
-                  className="border border-gray-300 min-w-[200px] h-[80px] max-h-[80px] overflow-y-auto overflow-x-hidden whitespace-normal px-3 py-2 align-top"
+                  className="border border-gray-300 min-w-[200px] h-[80px] max-h-[80px] px-3 py-2 align-top"
                 >
                   {item.text || item.choice?.label || '[ERROR]'}
                 </td>
